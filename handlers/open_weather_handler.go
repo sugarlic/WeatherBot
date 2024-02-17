e0e52eb6607be0d5fe0d9a6af2e77e68cedf7106
cherigra <cherigra@student.21-school.ru>
2024-02-17 23:58:37 +0300

    develop

--- handlers/open_weather_handler.go
+++ handlers/open_weather_handler.go
@@ -1,61 +1,69 @@
 package main
- 
+
 import (
-    "fmt"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-    "io/ioutil"
-    // "encoding/json"
 )
- 
+
 func MakeRequestToOpWether(city string) {
-    appID := "db7172c6a976b06502762e915d239656"
+	appID := "db7172c6a976b06502762e915d239656"
 
-    url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s",
-    city, appID)
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric",
+		city, appID)
 
-    response, err := http.Get(url);
-    if err != nil {
-        fmt.Println("Ошибка при отправке запроса:", err)
-        return
-    }
-    defer response.Body.Close()
-
-    body, err := ioutil.ReadAll(response.Body)
+	response, err := http.Get(url)
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Println("Ошибка при отправке запроса:", err)
+		return
+	}
+	defer response.Body.Close()
+
+	// body, err := io.ReadAll(response.Body)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	// log.Println(string(body))
+
+	var data map[string]interface{}
+	err = json.NewDecoder(response.Body).Decode(&data)
+	if err != nil {
+		fmt.Println("Ошибка при чтении ответа:", err)
+		return
 	}
 
-	log.Println(string(body))
+	for key, elem := range data {
+		log.Println(key, elem)
+	}
 
-    // var data map[string]interface{}
-    // err = json.NewDecoder(response.Body).Decode(&data)
-    // if err != nil {
-    //     fmt.Println("Ошибка при чтении ответа:", err)
-    //     return
-    // }
+	if main, ok := data["main"].([]interface{}); ok {
+		for _, elem := range main {
+			log.Println(elem)
+		}
+	}
 
+	// cities := make([]string, 0)
+	// if list, ok := data["list"].([]interface{}); ok {
+	//     for _, item := range list {
+	//         if city, ok := item.(map[string]interface{})["name"].(string); ok {
+	//             if sys, ok := item.(map[string]interface{})["sys"].(map[string]interface{}); ok {
+	//                 country := sys["country"].(string)
+	//                 cities = append(cities, fmt.Sprintf("%s (%s)", city, country))
+	//             }
+	//         }
+	//     }
+	// }
 
-    // cities := make([]string, 0)
-    // if list, ok := data["list"].([]interface{}); ok {
-    //     for _, item := range list {
-    //         if city, ok := item.(map[string]interface{})["name"].(string); ok {
-    //             if sys, ok := item.(map[string]interface{})["sys"].(map[string]interface{}); ok {
-    //                 country := sys["country"].(string)
-    //                 cities = append(cities, fmt.Sprintf("%s (%s)", city, country))
-    //             }
-    //         }
-    //     }
-    // }
-
-    // fmt.Println("city:", cities)
-    // if list, ok := data["list"].([]interface{}); ok && len(list) > 0 {
-    //     if item, ok := list[0].(map[string]interface{}); ok {
-    //         if cityID, ok := item["id"].(float64); ok {
-    //             fmt.Println("city_id=", int(cityID))
-    //         }
-    //     }
-    // }
+	// fmt.Println("city:", cities)
+	// if list, ok := data["list"].([]interface{}); ok && len(list) > 0 {
+	//     if item, ok := list[0].(map[string]interface{}); ok {
+	//         if cityID, ok := item["id"].(float64); ok {
+	//             fmt.Println("city_id=", int(cityID))
+	//         }
+	//     }
+	// }
 
 }
 
--- main.go
+++ main.go
@@ -1,44 +1,45 @@
 package main
 
 import (
-    "log"
-    "os"
-    // "example.com/m/configs"
+	"log"
+	"os"
 
-    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	// "example.com/m/configs"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
-    bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
-    if err != nil {
-        log.Panic(err)
-    }
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
 
-    bot.Debug = false
+	bot.Debug = false
 
-    log.Printf("Authorized on account %s", bot.Self.UserName)
+	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-    u := tgbotapi.NewUpdate(0)
-    u.Timeout = 60
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
 
-    updates := bot.GetUpdatesChan(u)
+	updates := bot.GetUpdatesChan(u)
 
-    for update := range updates {
-        if update.Message == nil { // ignore non-Message updates
-            continue
-        }
+	for update := range updates {
+		if update.Message == nil { // ignore non-Message updates
+			continue
+		}
 
-        msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 
-        switch update.Message.Text {
-        case "/Hello":
-            msg.Text = "Hello, I am test bot!"
-        default:
-            msg.Text = "Hello, there is my options: \n/Hello\n/Broadcast"
-        }
+		switch update.Message.Text {
+		case "/Hello":
+			msg.Text = "Hello, I am test bot!"
+		default:
+			msg.Text = "Hello, there is my options: \n/Hello\n/Broadcast"
+		}
 
-        if _, err := bot.Send(msg); err != nil {
-            log.Panic(err)
-        }
-    }
+		if _, err := bot.Send(msg); err != nil {
+			log.Panic(err)
+		}
+	}
 }
